0199ded5964047d997163873ccb4feebf0e3e5e5
CANNIBAL WH1YTE <90905815+wh1yterr@users.noreply.github.com>
2022-02-20 20:27:43 +0300

    Create task1.java

---
+++ task1.java
@@ -0,0 +1,10 @@
+public class hh {
+    public static void main(String[] args) {
+        if (args.length != 0) {
+            System.out.println("\tHello, "+ args[0] + "!");
+        }
+        else {
+            System.out.println("\tHello, World!");
+        }
+    }
+}
