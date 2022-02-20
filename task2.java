44d646549f037fb5cdfbb3cdcd3c458ae9dd8823
CANNIBAL WH1YTE <90905815+wh1yterr@users.noreply.github.com>
2022-02-20 20:28:24 +0300

    Create task2.java

---
+++ task2.java
@@ -0,0 +1,10 @@
+public class gg {
+    public static void main(String[] args) {
+        if (args.length != 0) {
+            System.out.print("\tВы ввели "+ args.length +" параметров!");
+        }
+        else {
+            System.out.print("\tВы не передавали параметров.");
+        }
+    }
+}
